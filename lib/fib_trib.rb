6a417d847ce862cb4d69c0a2206674589827ef59
Leland Husband <leland.husband@gmail.com>
2011-01-05 17:10:24 -0600

    refactoring

--- lib/fib_trib.rb
+++ lib/fib_trib.rb
@@ -7,19 +7,16 @@ class FibTrib
   end
   
   def fibonacci(position = 0)
-    if zero_or_one?(position)
-      1
-    else
-      pos_1 = position - 1
-      pos_2 = position - 2
-      fibonacci(pos_1) + fibonacci(pos_2)
-    end
+    return 2 if position == 2
+    return 1 if needs_one?(position)
+    pos_1 = position - 1
+    pos_2 = position - 2
+    fibonacci(pos_1) + fibonacci(pos_2)
   end
   
   def tribonacci(position = 0)
-    if zero_or_one?(position)
-      1
-    elsif position == 2
+    return 1 if needs_one?(position)
+    if position == 2
       position
     else 
       pos_1 = position - 1
@@ -33,5 +30,9 @@ class FibTrib
   
   def zero_or_one?(position)
     position == 0 || position == 1
+  end
+  
+  def needs_one?(position)
+    return true if position == 1 || position == 0
   end
 end
