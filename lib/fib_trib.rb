6c09ef3bd229a88f81338a79c4004671162773e1
Leland Husband <leland.husband@gmail.com>
2011-01-05 22:12:04 -0600

    trying to turn it into a gem

--- lib/fib_trib.rb
+++ lib/fib_trib.rb
@@ -1,26 +1,29 @@
-class FibTrib
-  
-  def sequence(n, pos = 0, sum = true)
-    base_array = [1,1,2]
-    return base_array[pos] if pos < 2 || pos == 2
-    iterator = pos - 2
-    
-    iterator.times do
-      base_array << get_current_sum(n, base_array)
-    end    
-    return base_array.last
-  end
-  
-  private
-  
-  def get_current_sum(n, base_array)
-    adjusted_n = n + 2
-    if adjusted_n > base_array.length || adjusted_n == base_array.length
-      base_array.inject { |k,v| k + v }
-    else
-      drop_amount = base_array.length - adjusted_n
-      base_array.drop(drop_amount).inject { |k,v| k + v }
-    end
-  end
+module FibTrib
 
+  class << self
+  
+    def sequence(n, pos = 0, sum = true)
+      base_array = [1,1,2]
+      return base_array[pos] if pos < 2 || pos == 2
+      iterator = pos - 2
+    
+      iterator.times do
+        base_array << get_current_sum(n, base_array)
+      end    
+      return base_array.last
+    end
+  
+    private
+  
+    def get_current_sum(n, base_array)
+      adjusted_n = n + 2
+      if adjusted_n > base_array.length || adjusted_n == base_array.length
+        base_array.inject { |k,v| k + v }
+      else
+        drop_amount = base_array.length - adjusted_n
+        base_array.drop(drop_amount).inject { |k,v| k + v }
+      end
+    end
+
+  end
 end
--- spec/unit/fib_trib_spec.rb
+++ spec/unit/fib_trib_spec.rb
@@ -2,101 +2,101 @@ require File.expand_path(File.dirname(__FILE__) + "/../../lib/fib_trib")
 
 describe FibTrib do
   
-  before(:each) do
-    @fib = FibTrib.new
-  end
-  
   context "fibonacci sequence" do
     
     it "#sequence should return 1 when no arguments are passed" do
-      @fib.sequence(0).should == 1
+      sequence(0).should == 1
     end
     
     it "#sequence should return 1 when pos = 1" do
-      @fib.sequence(0, 1).should == 1
+      sequence(0, 1).should == 1
     end
     
     it "#sequence should return 2 when pos = 2" do
-      @fib.sequence(0,2).should == 2
+      sequence(0,2).should == 2
     end
     
     it "#sequence should return 3 when pos = 3" do
-      @fib.sequence(0,3).should == 3
+      sequence(0,3).should == 3
     end
     
     it "#sequence should return 5 when pos = 4" do
-      @fib.sequence(0,4).should == 5
+      sequence(0,4).should == 5
     end
     
     it "#sequence should return 8 when pos = 5" do
-      @fib.sequence(0,5).should == 8
+      sequence(0,5).should == 8
     end
     
     it "#sequence should return 13 when pos = 6" do
-      @fib.sequence(0,6).should == 13
+      sequence(0,6).should == 13
     end
     
     it "#sequence should return 21 when pos = 7" do
-      @fib.sequence(0,7).should == 21
+      sequence(0,7).should == 21
     end
   end
 
   context "tribonacci sequence" do
     it "#sequence should return 1 when n = 1" do
-      @fib.sequence(1, 1).should == 1
+      sequence(1, 1).should == 1
     end
     
     it "#sequence should return 1 when n = 1 and pos = 1" do
-      @fib.sequence(1, 1).should == 1
+      sequence(1, 1).should == 1
     end
     
     it "#sequence should return 2 when n = 1 and pos = 2" do
-      @fib.sequence(1,2).should == 2
+      sequence(1,2).should == 2
     end
     
     it "#sequence should return 4 when n = 1 and pos = 3" do
-      @fib.sequence(1,3).should == 4
+      sequence(1,3).should == 4
     end
     
     it "#sequence should return 7 when n = 1 and pos = 4" do
-      @fib.sequence(1,4).should == 7
+      sequence(1,4).should == 7
     end
     
     it "#sequence should return 13 when n = 1 and pos = 5" do
-      @fib.sequence(1,5).should == 13
+      sequence(1,5).should == 13
     end
   end
 
   context "quadronacci sequence" do
     it "#sequence should return 1 when n = 2" do
-      @fib.sequence(2).should == 1
+      sequence(2).should == 1
     end
     
     it "#sequence should return 1 when n = 2 and pos = 1" do
-      @fib.sequence(2,1).should == 1
+      sequence(2,1).should == 1
     end
     
     it "#sequence should return 2 when n = 2 and pos = 2" do
-      @fib.sequence(2,2).should == 2
+      sequence(2,2).should == 2
     end
     
     it "#sequence should return 4 when n = 2 and pos = 3" do
-      @fib.sequence(2,3).should == 4
+      sequence(2,3).should == 4
     end
     
     it "#sequence should return 8 when n = 2 and pos = 4" do
-      @fib.sequence(2,4).should == 8
+      sequence(2,4).should == 8
     end
     
     it "#sequence should return 15 when n = 2 and pos = 5" do
-      @fib.sequence(2,5).should == 15
+      sequence(2,5).should == 15
     end
 
   end
 
   context "pentanacci sequence" do
     it "#sequence should return 32 when n = 4 and pos = 6" do
-     @fib.sequence(4,8).should == 125      
+     sequence(4,8).should == 125      
     end
+  end
+  
+  def sequence(n,pos = 0)
+    FibTrib.sequence(n,pos)
   end
 end
